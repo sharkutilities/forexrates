f01850fb4c3a99b0e5f9bd3dfc93134ec3098fb4
E33605 <debmalyapramanik.005@gmail.com>
2026-02-13 10:05:52 +0530

    ✨🚧 added setup.py and pyproject.toml for editable installation

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="forexrates",
+    version="0.0.1.dev0",
+    description="A unified codebase for fetching Foreign Exchange Rates from different API sources",
+    author="sharkutilities",
+    url="https://github.com/sharkutilities/forexrates",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.24.4",
+        "pandas==1.5.3",
+        "psycopg2==2.9.10",
+        "SQLAlchemy==1.4.54",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
