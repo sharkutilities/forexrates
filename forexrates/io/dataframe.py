a62eda40e5ae11c25866f98d645f4f2f469af0ba
E33605 <debmalyapramanik.005@gmail.com>
2026-02-17 10:36:25 +0530

    💣🐛 remove tqdm dependency from project - external users are given the option to use their own custom progress bar

--- forexrates/io/dataframe.py
+++ forexrates/io/dataframe.py
@@ -6,7 +6,6 @@ Operations to Parse Data as :mod:``pandas.DataFrame`` Object
 
 import datetime as dt
 
-from tqdm import tqdm as TQ
 from typing import Iterable
 
 import pandas as pd
@@ -103,7 +102,7 @@ class ExchangeRatesIO(BaseIO):
         datecolumn = kwargs.get("datecolumn", "effective_date")
 
         frames = []
-        for data in TQ(self.data, desc = "Parsing Records..."):
+        for data in self.data:
             base = data["base"]
             date = data["date"]
 
