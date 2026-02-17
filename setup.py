a36322f1cd5ddd07517c051cfcf05ad213c77034
E33605 <debmalyapramanik.005@gmail.com>
2026-02-17 11:51:12 +0530

    🐛 greenlet==3.3.1 requires python >= 3.10
    - updated python version requirement across whole project

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
@@ -69,5 +68,5 @@ setup(
         # keywords for finding the package relevant to usecases::
         "wrappers", "data science", "data analysis", "data scientist", "data analyst"
     ],
-    python_requires = ">=3.9"
+    python_requires = ">=3.10"
 )
