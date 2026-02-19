0b5fa3335cfad38a2759f4ee68ca601174e49863
E33605 <debmalyapramanik.005@gmail.com>
2026-02-19 15:34:18 +0530

    🐛 update version number in package init file

--- forexrates/__init__.py
+++ forexrates/__init__.py
@@ -9,7 +9,7 @@ to fetch foreign exchange rates.
 
 # ? package follows https://peps.python.org/pep-0440/
 # ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
-__version__ = "v0.0.1.dev0"
+__version__ = "v1.0.0"
 
 # init-time options registrations
 from forexrates import io
